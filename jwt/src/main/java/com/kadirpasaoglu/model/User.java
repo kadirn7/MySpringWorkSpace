319b1bbadb570b8ad177affa4619745c0a7986e6
kadirn7 <kadirneidik398@gmail.com>
2025-03-02 18:42:07 +0300

    d

--- jwt/src/main/java/com/kadirpasaoglu/model/User.java
+++ jwt/src/main/java/com/kadirpasaoglu/model/User.java
@@ -3,7 +3,6 @@ package com.kadirpasaoglu.model;
 
 
 import java.util.Collection;
-import java.util.ArrayList;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -30,40 +29,9 @@ public class User implements UserDetails {
 	private Long id;
 	private String username;
 	private String password;
-	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// Basit bir yetkilendirme için boş liste dönebiliriz
-		return new ArrayList<>();
-	}
-	
-	@Override
-	public String getPassword() {
-		return password;
-	}
-	
-	@Override
-	public String getUsername() {
-		return username;
-	}
-	
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
-	
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
-	
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-	
-	@Override
-	public boolean isEnabled() {
-		return true;
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
